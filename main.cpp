923a922cc6d733a05d791d5df4f2fa4f14f9a7e6
Fabio Deo <36317183+Fabio752@users.noreply.github.com>
2018-02-20 21:30:14 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
 #include <ctime>
 using namespace std;
 
-
 void print_grid(vector<char> grid);
 void insert_choose(vector<char>& grid, char choose, int column);
 bool win(vector<char> grid);
@@ -23,35 +22,43 @@ bool complete_grid(vector<char> grid);
 bool valid_insert(vector<char> grid, int column);
 
 int main(){
+    //initial conditions of the variables
+    bool gameover = false;
+    char winner;
+    bool comp_grid = complete_grid(grid);
+    bool valid_input;
     int num_of_rows = 6;
     int num_of_col = 7;
     
-    
+    //initial grid
     vector<char> grid;
     for (int i = 0; i < num_of_rows; i++)
         for (int j = 0; j < num_of_col; j++)
             grid.push_back('_');
     
-    bool gameover = false;
-    char winner;
-    bool comp_grid = complete_grid(grid);
-    bool valid_input;
+    //gameplay
     while(!gameover && !comp_grid){
         print_grid(grid);
-
+        
+        // Player 1 turn
         int col_input_piece;
         cout << "Player X, choose the number of column you want to insert your piece (1-7)" <<endl;
+        
+        //ensure that the input is valid
         while (!(cin >> col_input_piece) || ((col_input_piece < 1 || col_input_piece > 7) && !valid_input)) {
             std::cin.clear(); //clear bad input flag
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
             cout <<"Please insert a valid column number (1-7)" <<endl;
             valid_input = valid_insert(grid, col_input_piece);
         }
-
+        
         insert_choose(grid, 'x' , col_input_piece);
         gameover = win(grid);
-        comp_grid = complete_grid(grid);
         
+        if (!gameover)
+            comp_grid = complete_grid(grid);
+        
+        //Player 2 turn
         if (!gameover && !comp_grid){
             print_grid(grid);
 
@@ -66,23 +73,27 @@ int main(){
             
             insert_choose(grid, 'o', col_input_piece);
             gameover = win(grid);
-            comp_grid = complete_grid(grid);
             
             if (gameover)
                 winner = 'o';
+            else
+                comp_grid = complete_grid(grid);
         }
         else
             winner = 'x';
     }
     
     print_grid(grid);
+    
     if (gameover)
         cout <<"The winner is Player: " <<winner <<endl;
     else
         cout <<"It is a draw!" <<endl;
+    
     return 0;
 }
 
+//print the updated grid
 void print_grid(vector<char> grid){
     int num_of_row = 6;
     int num_of_col = 7;
@@ -92,27 +103,27 @@ void print_grid(vector<char> grid){
             cout <<grid[num_of_col * i + j] <<" ";
         cout <<endl;
     }
+    
 }
 
+//insert the piece in the input column
 void insert_choose(vector<char>& grid, char choose, int column){
     int num_of_col = 7;
+    
     for (int i = column - 1; i < grid.size(); i = i + num_of_col){
         
         if (i + num_of_col > grid.size())
             grid[i] = choose;
-        
-        
         else
             if (grid[i + num_of_col] != '_'){
                 grid[i] = choose;
                 break;
             }
-    }
-        
-    
-    
+      
+    }   
 }
 
+//check if a player won and return a bool
 bool win(vector<char> grid){
     int num_of_row = 6;
     int num_of_col = 7;
@@ -123,42 +134,41 @@ bool win(vector<char> grid){
             
             //checking on the col
             if (index < 20)
-                if (grid[index] == grid[index + num_of_col] && grid[index] != '_' && grid[index] == grid[index + 2 * num_of_col] && grid[index] == grid[index + 3 * num_of_col])
-                    
+                if (grid[index] == grid[index + num_of_col] && grid[index] != '_' && grid[index] == grid[index + 2 * num_of_col] && grid[index] == grid[index + 3 * num_of_col] 
                     return true;
-            
-            
+           
             //checking the row
             if (j < 4)
                 if (grid[index] == grid[index + 1]  && grid[index] != '_' && grid[index] == grid[index + 2] && grid[index] == grid[index + 3])
                     return true;
             
-            
             //check the left to right diagonal
             if ( j < 4 && index < 20)
                 if (grid[index] == grid[index + num_of_col + 1]  && grid[index] != '_' && grid[index] == grid[index + 2 * num_of_col + 2] && grid[index] == grid[index + 3 * num_of_col + 3])
                     return true;
-            
+                    
             //check the right to left diagonal
             if ( j > 2 && index < 20)
                 if (grid[index] == grid[index + num_of_col - 1]  && grid[index] != '_' && grid[index] == grid[index + 2 * num_of_col - 2] && grid[index] == grid[index + 3 * num_of_col - 3])
                     return true;
         
         }
-        
     }
+                    
     return false;
 }
 
+//if the grid is complete thus the game is a draw returns true, otherwise 0
 bool complete_grid(vector<char> grid){
     
     for (int i = 0; i < grid.size(); i++)
         if (grid[i] == '_')
             return false;
     return true;
+    
 }
-
-
+  
+// returns a bool set to true if the input column is full, empty otherwise
 bool valid_insert(vector<char> grid, int column){
     if (grid[column - 1] == '_')
         return true;
